f3bace57530dce85765c0fe1cea192ffcdb83797
julienbarrier <1346858+julienbarrier@users.noreply.github.com>
2021-08-15 18:24:59 +0100

    init

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='mesoscopy',
+      version='0.0.1',
+      description='Library of utils to run experiments in mesoscopic physics',
+      url='https://github.com/julienbarrier/mesoscopy',
+      author='Julien Barrier',
+      author_email='julien.barrier@manchester.ac.uk',
+      classifiers=[
+          "Intended Audience :: Science/Research",
+          "Programming Language :: Python :: 3 :: Only",
+          "License :: MIT License",
+          "Topic :: Scientific/Engineering",
+      ]
+      license='MIT',
+      packages=find_packages(),
+      python_requires=">=3.9",
+      install_requires=[
+          "matplotlib>=3.4.0",
+          "pandas>=1.3.0",
+          "pyqt5>5.15.0",
+          "numpy>=1.21.0"
+      ]
+      zip_safe=False)
