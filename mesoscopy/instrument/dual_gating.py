54527b68b062403595e836c8743b6a40322e3536
Julien Barrier <1346858+julienbarrier@users.noreply.github.com>
2021-09-09 10:57:22 +0100

    dual sweep with max_rate

--- mesoscopy/instrument/dual_gating.py
+++ mesoscopy/instrument/dual_gating.py
@@ -2,103 +2,178 @@
 Keithley 2600
 """
 
-from typing import Tuple
+from typing import Tuple, Optional
 from scipy.constants import e, epsilon_0
 
-from qcodes.instruments.parameters import _BaseParameter, \
-    ParameterWithSetpoints
-from qcodes.utils.helpers import abstractmethod
+from qcodes.instrument.parameter import _BaseParameter, Parameter
 
 
-class DensityParameter(ParameterWithSetpoints):
-    """ create a density parameter for maps at constant displacement"""
+class DensityParameter(Parameter):
+    """ density parameter for maps at constant density
+
+    Args:
+        ``gates``: tuple[Parameter, Parameter]. the parameter to sweep for top
+            gate and back gate, respectively.
+        ``capacitances``: tuple[float, float]. capacitance for top gate and
+            back gate, respectively, in F/m
+        ``lock_D``: bool. If true, when we sweep the parameter, it will be done
+            at the displacement when parameter is initialised. If false, it
+            will follow the density of previous measurement, with possible
+            drifts.
+    """
     def __init__(self, name: str,
                  gates: Tuple[_BaseParameter, _BaseParameter],
                  capacitances: Tuple[float, float],
-                 *args, **kwargs) -> None:
-        super().__init__(name=name,
-                         setpoints=list[gates],
-                         *args, **kwargs)
+                 lock_D: Optional[bool] = False,
+                 *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
         self._vtg = gates[0]
         self._vbg = gates[1]
         self._ctg = capacitances[0]
         self._cbg = capacitances[1]
+        self._D = (self._ctg/self._vtg() - self._cbg/self._vbg())/2/epsilon_0
+        self._lockD = lock_D
+        self._instrument = gates[0].root_instrument
 
     @property
-    def vtg(self):
-        for param in self._vtg:
-            return param.get()
-
-    @vtg.setter
-    def vtg(self, value):
-        for param in self._vtg:
-            param.set(value)
-
-    @property
-    def vbg(self):
-        for param in self._vbg:
-            return param.get()
-
-    @vbg.setter
-    def vbg(self, value):
-        for param in self._vbg:
-            param.set(value)
-
     def D(self):
-        return (self._ctg*self.vtg() - self._cbg*self.vbg())/2/epsilon_0
+        return(self._ctg*self._vtg() - self._cbg*self._vbg())/2/epsilon_0
 
-    @abstractmethod
+    @property
+    def max_rate(self):
+        if self._vtg.max_rate():
+            vtg_maxrate = self._vtg.max_rate()*self._ctg
+        elif self._vtg._instrument.max_rate():
+            vtg_maxrate = self._vtg._instrument.max_rate()*self._ctg
+        else:
+            vtg_maxrate = 0
+
+        if self._vbg.max_rate():
+            vbg_maxrate = self._vbg.max_rate()*self._cbg
+        elif self._vbg._instrument.max_rate():
+            vbg_maxrate = self._vbg._instrument.max_rate()*self._cbg
+        else:
+            vbg_maxrate = 0
+
+        if not vtg_maxrate:
+            return vbg_maxrate/e
+        elif not vbg_maxrate:
+            return vtg_maxrate/e
+        else:
+            return min(vtg_maxrate, vbg_maxrate)/e
+
     def get_raw(self):
-        return (self._ctg*self.vtg() + self._cbg*self.vbg())/e
+        return (self._ctg*self._vtg() + self._cbg*self._vbg())/e
 
-    @abstractmethod
     def set_raw(self, value):
-        self.vtg((e*value + 2*epsilon_0*self.D)/2/self._ctg)
-        self.vbg((e*value - 2*epsilon_0*self.D)/2/self._cbg)
+        if self._lockD:
+            displacement = self._D
+        else:
+            displacement = self.D
+        self._vtg((e*value + 2*epsilon_0*displacement)/2/self._ctg)
+        self._vbg((e*value - 2*epsilon_0*displacement)/2/self._cbg)
 
 
-class DisplacementParameter(ParameterWithSetpoints):
-    """ create a displacement parameter for maps at constant density"""
+class DisplacementParameter(Parameter):
+    """ displacement parameter for maps at constant density
+
+    Args:
+        ``gates``: tuple[Parameter, Parameter]. the parameter to sweep for top
+            gate and back gate, respectively.
+        ``capacitances``: tuple[float, float]. capacitance for top gate and
+            back gate, respectively, in F/m
+        ``lock_n``: bool. If true, when we sweep the parameter, it will be done
+            at the density when parameter is initialised. If false, it will
+            follow the density of previous measurement, with possible drifts
+    """
     def __init__(self, name: str,
                  gates: Tuple[_BaseParameter, _BaseParameter],
                  capacitances: Tuple[float, float],
-                 *args, **kwargs) -> None:
-        super().__init__(name=name,
-                         setpoints=list[gates],
-                         *args, **kwargs)
+                 lock_n: Optional[bool] = False,
+                 *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
         self._vtg = gates[0]
         self._vbg = gates[1]
         self._ctg = capacitances[0]
         self._cbg = capacitances[1]
+        self._n = (self._ctg/self._vtg() + self._cbg/self._vbg())/e
+        self._lockn = lock_n
+        self._instrument = gates[0].root_instrument
 
     @property
-    def vtg(self):
-        for param in self._vtg:
-            return param.get()
-
-    @vtg.setter
-    def vtg(self, value):
-        for param in self._vtg:
-            param.set(value)
-
-    @property
-    def vbg(self):
-        for param in self._vbg:
-            return param.get()
-
-    @vbg.setter
-    def vbg(self, value):
-        for param in self._vbg:
-            param.set(value)
-
     def n(self):
-        return (self._ctg*self.vtg() + self._cbg*self.vbg())/e
+        return(self._ctg*self._vtg() + self._cbg*self._vbg())/e
 
-    @abstractmethod
+    @property
+    def max_rate(self):
+        if self._vtg.max_rate():
+            vtg_maxrate = self._vtg.max_rate()*self._ctg
+        elif self._vtg._instrument.max_rate():
+            vtg_maxrate = self._vtg._instrument.max_rate()*self._ctg
+        else:
+            vtg_maxrate = 0
+
+        if self._vbg.max_rate():
+            vbg_maxrate = self._vbg.max_rate()*self._cbg
+        elif self._vbg._instrument.max_rate():
+            vbg_maxrate = self._vbg._instrument.max_rate()*self._cbg
+        else:
+            vbg_maxrate = 0
+
+        if not vtg_maxrate:
+            return vbg_maxrate/2/epsilon_0
+        elif not vbg_maxrate:
+            return vtg_maxrate/2/epsilon_0
+        else:
+            return min(vtg_maxrate, vbg_maxrate)/2/epsilon_0
+
     def get_raw(self):
-        return (self._ctg*self.vtg() - self._cbg*self.vbg())/2/epsilon_0
+        return (self._ctg*self._vtg() - self._cbg*self._vbg())/2/epsilon_0
 
-    @abstractmethod
     def set_raw(self, value):
-        self.vtg((e*self.n + 2*epsilon_0*value)/2/self._ctg)
-        self.vbg((e*self.n - 2*epsilon_0*value)/2/self._cbg)
+        if self._lockn:
+            density = self._n
+        else:
+            density = self.n
+        self._vtg((e*density + 2*epsilon_0*value)/2/self._ctg)
+        self._vbg((e*density - 2*epsilon_0*value)/2/self._cbg)
+
+
+class LinearParameter(Parameter):
+    """a parameter two fix a linear relation between two parameters as:
+        ``dependent_param = m * primary_param + p``
+
+
+    Args:
+        ``primary_param``: Parameter to sweep
+        ``dependent_param``: Parameter that will be swept as a function of
+            ``primary_param``
+        ``m``: float, coefficient for the linear relation
+        ``p``: float, intercept in the linear relation
+    """
+    def __init__(self, name: str,
+                 primary_param: _BaseParameter,
+                 dependent_param: _BaseParameter,
+                 m: float,
+                 p: float,
+                 *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+        self._x = primary_param
+        self._y = dependent_param
+        self._m = m
+        self._p = p
+
+        @property
+        def max_rate(self):
+            if self._x.max_rate():
+                return self._x.max_rate()
+            elif self._x._instrument.max_rate():
+                return self._x._instrument.max_rate()
+            else:
+                return None
+
+        def get_raw(self):
+            return self._x
+
+        def set_raw(self, value):
+            self._y(self._m * self._x + self._p)
