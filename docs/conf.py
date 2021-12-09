088060221ad07d5ab02c17612f28bf9f7c91e974
Julien Barrier <julien.barrier@espci.org>
2021-12-09 22:05:46 +0000

    doc import mesoscopy

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import sphinx_rtd_theme
-import mesoscopy
+import ..mesoscopy
 
 # -- Project information -----------------------------------------------------
 
